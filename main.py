01bdb985c3cf6376715ea91a85d1771bc0228e69
dev eloper <dev@email.com>
2022-03-19 20:35:51 +0200

    moved answers and questions into files

--- main.py
+++ main.py
@@ -1,48 +1,38 @@
 '''
     this is a quiz game
+    
+    part 1: creating the game
     started: 19/3/2022 15:36
     finished: 19/3/2022 17:33
+
+    part 2
+    started: 19/3/2022 20:03
+    finished: 19/3/2022 XXXXXX
 '''
+import json
+
+#read answers from file, convert them into a tuple and appened them into the list
+answers = []
+with open('answers.txt') as answers_file:
+    for i in answers_file:
+        answers.append(tuple(i.rstrip('\n').split(",")))
 
 #question source https://www.mentimeter.com/blog/audience-energizers/55-free-trivia-and-fun-quiz-question-templates
-questions = {
-"What country has the highest life expectancy?":"B",
-"Where would you be if you were standing on the Spanish Steps?":"A",
-"Which language has the more native speakers: English, Portuguese, Spanish or Mandarin?":"D",
-"What is the most common surname in the United States?":"D",
-"What disease commonly spread on pirate ships?":"B",
-"Who was the Ancient Greek God of the Sun? ":"C",
-"What was the name of the crime boss who was head of the feared Chicago Outfit?":"A",
-"What year was the United Nations established?":"B",
-"Who has won the most total Academy Awards?":"B",
-"What artist has the most streams on Spotify?":"C",
-} #TODO: move questions to json file
-
-answers = [
-("A)Russia","B)Hong Kong","C)Australia","D)Taiwan"),
-("A)Rome","B)Greece","C)Madrid","D)Morocco"),
-("A)Mandarin","B)English","C)Portuguese","D)Spanish"),
-("A)White","B)Mcgregor","C)Kohen","D)Smith"),
-("A)Chickenpox","B)Scurvy","C)Influenza","D)Covid-19"),
-("A)Zeus","B)Poseidon","C)Apollo","D)Artemis"),
-("A)Al Capone","B)Bugsy Siegel","C)Lucky Luciano","D)Tony Accardo"),
-("A)1939","B)1945","C)1918","D)1948"),
-("A)Ethan Coen","B)Walt Disney","C)Robert Amram","D)Peter Farrelly"),
-("A)Taylor Swift","B)The Weeknd","C)Drake","D)Beyonce")
-] #TODO: move to a file
+with open('questions.json') as questions_json:
+    questions = json.load(questions_json)
 
 def restart_game():
     score = 0
     questionNum = 0
 
-    for i in questions.keys():
+    for question in questions.keys():
         answer=None
 
         print("\n------------------------")
-        print(i+"\n")
+        print(question+"\n")
         print(*answers[questionNum],sep="\n") # * is called kwargs
 
-        score += check_answer(answer:=input("what is your answer? ").upper(),i) # := is called walrus (working on python 3.8+)
+        score += check_answer(answer:=input("what is your answer? ").upper(),question) # := is called walrus (working on python 3.8+)
         questionNum += 1
     
     show_score(score)
@@ -69,3 +59,6 @@ def main():
 
 
 main()
+
+
+        
