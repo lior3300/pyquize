36f3dcac0783550eabc8bcf470fad77eca77fe7a
dev eloper <dev@email.com>
2022-03-20 14:05:27 +0200

    added so the answers will have random order

--- main.py
+++ main.py
@@ -5,17 +5,26 @@
     started: 19/3/2022 15:36
     finished: 19/3/2022 17:33
 
-    part 2
+    part 2: moved questions and answers to files
     started: 19/3/2022 20:03
-    finished: 19/3/2022 XXXXXX
+    finished: 19/3/2022 20:37
+
+    part 3:
+    started: 20/3/2022 13:04
+    finished: 20/3/2022 14:04
 '''
 import json
+import random
+from operator import concat
 
 #read answers from file, convert them into a tuple and appened them into the list
 answers = []
 with open('answers.txt') as answers_file:
     for i in answers_file:
-        answers.append(tuple(i.rstrip('\n').split(",")))
+        ansr = tuple(i.rstrip('\n').split(","))
+        randansr = tuple(random.sample(ansr, len(ansr)))
+        randansr = tuple(map(concat, ("A)","B)","C)","D)"), randansr))
+        answers.append(randansr)
 
 #question source https://www.mentimeter.com/blog/audience-energizers/55-free-trivia-and-fun-quiz-question-templates
 with open('questions.json') as questions_json:
@@ -24,26 +33,28 @@ with open('questions.json') as questions_json:
 def restart_game():
     score = 0
     questionNum = 0
-
+    
     for question in questions.keys():
         answer=None
-
+        
         print("\n------------------------")
         print(question+"\n")
         print(*answers[questionNum],sep="\n") # * is called kwargs
-
-        score += check_answer(answer:=input("what is your answer? ").upper(),question) # := is called walrus (working on python 3.8+)
+        
+        score += check_answer(answer:=input("what is your answer? ").upper(),question,questionNum) # := is called walrus (working on python 3.8+)
         questionNum += 1
     
     show_score(score)
         
-def check_answer(answer,question):
-    if questions.get(question) == answer:
-        print("\nCorrect!")
-        return 1
-    else:
-        print("\nwrong!")
-        return 0
+def check_answer(answer,question,questionNum):
+    
+    if answer in ["A","B","C","D"]:
+        if questions.get(question) == answers[questionNum][ord(answer)-ord("A")][2:]:
+            print("\nCorrect!")
+            return 1
+            
+    print("\nwrong!")
+    return 0
 
 def show_score(score):
     percent=(score/len(questions)*100)
